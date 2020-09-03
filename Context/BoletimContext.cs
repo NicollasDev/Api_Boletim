ab15c7cdf8098df92ae2f4d44e859fd5bc1068ce
NicollasDileo06 <nicgato2013@gmail.com>
2020-09-03 19:21:52 -0300

    concluindo

---
+++ Context/BoletimContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiBoletim.Context
+{
+    public class BoletimContext
+    {
+        SqlConnection con = new SqlConnection();
+        public BoletimContext()
+        {
+            con.ConnectionString = @"Data Source=DESKTOP-DC44E8T\SQLEXPRESS;Initial Catalog=boletim;Persist Security Info=True;User ID=sa;Password=sa132";
+        }
+
+        public SqlConnection Conectar()
+        {
+            if(con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+            return con;
+        }
+
+        public void Desconectar()
+        {
+            if(con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();   
+            }
+        }
+
+    }
+}
